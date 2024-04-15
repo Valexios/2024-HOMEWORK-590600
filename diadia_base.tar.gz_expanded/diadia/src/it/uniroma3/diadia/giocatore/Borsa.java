792b12c4e1483918b59cd3ddf865966dd369ec3a
Valexios <125564845+Valexios@users.noreply.github.com>
2024-04-15 16:18:50 +0200

    Add files via upload

---
+++ diadia_base.tar.gz_expanded/diadia/src/it/uniroma3/diadia/giocatore/Borsa.java
@@ -0,0 +1,97 @@
+package it.uniroma3.diadia.giocatore;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+public class Borsa {
+	public final static int DEFAULT_PESO_MAX_BORSA = 10;
+	
+	
+	private Attrezzo[] attrezzi;
+	private int numeroAttrezzi;
+	private int pesoMax;
+	
+	public Borsa() {
+		this(DEFAULT_PESO_MAX_BORSA);
+	}
+	
+	public Borsa(int pesoMax) {
+		this.pesoMax = pesoMax;
+		this.attrezzi = new Attrezzo[10]; // speriamo bastino...
+		this.numeroAttrezzi = 0;
+	}
+	
+	public boolean addAttrezzo(Attrezzo attrezzo) {
+		if(attrezzo==null)
+			return false;
+		if (this.getPeso() + attrezzo.getPeso() > this.getPesoMax())
+			return false;
+		if (this.numeroAttrezzi==10)
+			return false;
+		this.attrezzi[this.numeroAttrezzi] = attrezzo;
+		this.numeroAttrezzi++;
+		return true;
+	}
+	
+	public int getPesoMax() {
+		return pesoMax;
+	}
+	
+	public Attrezzo getAttrezzo(String nomeAttrezzo) {
+		Attrezzo a = null;
+		boolean continua = true;
+		for (int i= 0; i<this.numeroAttrezzi; i++)
+			if (this.attrezzi[i].getNome().equals(nomeAttrezzo)&&continua) {
+				a = attrezzi[i];
+				continua=false;
+			}
+		return a;
+	}
+	
+	public int getPeso() {
+		int peso = 0;
+		for (int i= 0; i<this.numeroAttrezzi; i++)
+			peso += this.attrezzi[i].getPeso();
+		return peso;
+	}
+	
+	public boolean isEmpty() {
+		return this.numeroAttrezzi == 0;
+	}
+	
+	public boolean hasAttrezzo(String nomeAttrezzo) {
+		return this.getAttrezzo(nomeAttrezzo)!=null;
+	}
+	
+	public Attrezzo removeAttrezzo(String nomeAttrezzo) {
+		if(!this.hasAttrezzo(nomeAttrezzo))
+			return null;
+		boolean trovato = false;
+		int num=this.numeroAttrezzi;
+		Attrezzo a = this.getAttrezzo(nomeAttrezzo);
+		Attrezzo[] NewBorsa= new Attrezzo[DEFAULT_PESO_MAX_BORSA];
+		for (int i=0,k=0; i<num; i++) {
+			if(!this.attrezzi[i].getNome().equals(nomeAttrezzo)||trovato) {
+				NewBorsa[k]=attrezzi[i];
+				k++;
+			}
+			else{
+				this.numeroAttrezzi--;
+				trovato = true;
+			}
+	    }
+		this.attrezzi=NewBorsa;	
+		return a;
+	}
+	
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		if (!this.isEmpty()) {
+			s.append("Contenuto borsa ("+this.getPeso()+"kg/"+this.getPesoMax()+"kg): ");
+			for (int i= 0; i<this.numeroAttrezzi; i++)
+				s.append(attrezzi[i].toString()+" ");
+		}
+		else
+			s.append("Borsa vuota");
+		return s.toString();
+	}
+}
